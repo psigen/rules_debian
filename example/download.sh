7f0cfa4ef71b4862e5c7d4299177b76336c103d7
Pras Velagapudi <psigen@gmail.com>
2020-05-01 23:57:44 -0400

    Let's test a download rule.

---
+++ example/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for i in $(apt-cache depends python | grep -E 'Depends|Recommends|Suggests' | cut -d ':' -f 2,3 | sed -e s/'<'/''/ -e s/'>'/''/); do
+    apt-get download $i 2>>errors.txt
+done
